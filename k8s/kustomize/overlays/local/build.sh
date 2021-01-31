44c091f3af3019aa67c182b11e2a8d860ac283be
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2021-01-31 11:58:54 +0900

    update ci

--- k8s/kustomize/overlays/local/build.sh
+++ k8s/kustomize/overlays/local/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-cd $(dirname $0)
 
 # k8s-request-counter-emitter/k8s/kustomize/overlays/local にいる状態で実行する
+cd $(dirname $0)
 
-#IMAGE_NAME="$1"
-#kustomize edit set image app-image="${IMAGE_NAME}"
-kustomize edit set image app-image="pokotyan/k8s-request-counter-emitter:latest"
+IMAGE_NAME="$1"
+kustomize edit set image app-image="${IMAGE_NAME}"
+#kustomize edit set image app-image="pokotyan/k8s-request-counter-emitter:latest"
 kustomize build . >../../../manifest/app/app.yml
+
+git commit -am "Update image to $IMAGE_NAME"
